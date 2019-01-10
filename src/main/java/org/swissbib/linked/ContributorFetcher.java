53556ee116ea815787d2720efbbb02023542d768
Sebastian Schüpbach <sebastian.schuepbach@unibas.ch>
2019-01-10 18:12:59 +0100

    Get source of results instead of respective fields
    Signed-off-by: Sebastian Schüpbach <sebastian.schuepbach@unibas.ch>

--- src/main/java/org/swissbib/linked/ContributorFetcher.java
+++ src/main/java/org/swissbib/linked/ContributorFetcher.java
@@ -3,15 +3,11 @@ package org.swissbib.linked;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.search.SearchHit;
-import org.elasticsearch.search.sort.FieldSortBuilder;
 import org.elasticsearch.search.sort.SortOrder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 
 import static org.elasticsearch.index.query.QueryBuilders.existsQuery;
 
@@ -26,24 +22,31 @@ class ContributorFetcher {
     }
 
     HashMap<String, HashSet<String>> execute(String index) {
+        HashMap<String, HashSet<String>> results = new HashMap<>();
+        HashSet<String> pers = new HashSet<>();
+        HashSet<String> orga = new HashSet<>();
+        results.put("pers", pers);
+        results.put("orga", orga);
         SearchResponse scrollResp = esClient.client
                 .prepareSearch(index)
-                .addSort(FieldSortBuilder.DOC_FIELD_NAME, SortOrder.ASC)
+                .addSort("_doc", SortOrder.ASC)
                 .setTypes("bibliographicResource")
                 .setFetchSource("dct:contributor", null)
                 .setScroll(new TimeValue(60000))
                 .setQuery(existsQuery("dct:contributor"))
                 .setSize(1000)
                 .get();
-        HashMap<String, HashSet<String>> results = new HashMap<>();
-        HashSet<String> pers = new HashSet<>();
-        HashSet<String> orga = new HashSet<>();
-        results.put("pers", pers);
-        results.put("orga", orga);
         do {
             for (SearchHit hit : scrollResp.getHits().getHits()) {
-                for (Object contributor : hit.getField("dct:contributor").getValues()) {
-                    List<String> splitted = Arrays.asList(contributor.toString().split("/"));
+                String rawContributors = hit.getSource().get("dct:contributor").toString();
+                ArrayList<String> contributors = new ArrayList<>();
+                if (rawContributors.startsWith("[")) {
+                    contributors.addAll(Arrays.asList(rawContributors.substring(1, rawContributors.length() - 1).split(", ")));
+                } else {
+                    contributors.add(rawContributors);
+                }
+                for (String contributor : contributors) {
+                    List<String> splitted = Arrays.asList(contributor.split("/"));
                     String id = splitted.get(splitted.size() - 1);
                     if (splitted.get(splitted.size() - 2).equals("person")) {
                         pers.add(id);
